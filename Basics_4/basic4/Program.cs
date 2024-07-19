a4e245f853af8b73dcf2cd08d9c4c66818cd3d21
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-07-20 04:16:45 +0500

    Ad Vs Solution to the project

---
+++ Basics_4/basic4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string dayweek;
+dayweek = Console.ReadLine();
+string result = dayweek switch
+{
+"Dushanba"=>$"{dayweek}-Monday",
+"Seshanba"=>$"{dayweek}-Tuesday",
+"Chorshanba"=>$"{dayweek}-Wednesday",
+"Payshanba"=>$"{dayweek}-Thursday",
+"Juma"=>$"{dayweek}-Friday",
+"Shanba"=>$"{dayweek}-Saturday",
+"Yakshanba"=>$"{dayweek}-Sunday"
+};
+Console.WriteLine(result);
