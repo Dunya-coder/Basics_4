f88576462677e8f7df3c3670f8f7095dc5291868
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-07-20 04:35:35 +0500

    Fineshed project

--- Basics_4/basic4/Program.cs
+++ Basics_4/basic4/Program.cs
@@ -1,6 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//1
+int x_1;
+x_1 = Convert.ToInt32(Console.ReadLine());
+string str;
+str = Console.ReadLine();
+if(x_1>str.Length)
+{
+    Console.WriteLine(str.ToUpper());
+}
+else
+    Console.WriteLine(str.ToLower());
+//2
+int x = 15, y = 10;
 
+if(x>y)
+{ Console.WriteLine("x is greater than y"); }
+if(x<y)
+{ Console.WriteLine("y is greater than x"); }
+if(x==y)
+{ Console.WriteLine("x equal to y"); }
+else
+    Console.WriteLine("x and y are not comparable");
+//3
 string dayweek;
 dayweek = Console.ReadLine();
 string result = dayweek switch
